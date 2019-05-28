f54e0ef82b6eb7789140825b49c5b6d2eed4a6a4
Santiago1732 <49510623+Santiago1732@users.noreply.github.com>
2019-05-28 01:04:34 -0300

    Create tiposDeDatos.cpp

---
+++ 02-TiposDeDatos/tiposDeDatos.cpp
@@ -0,0 +1,43 @@
+/*
+* 02-TiposDeDatos
+* Santiago Oliveira 168 606 9
+* 28/05/2019
+*/
+#include <string>
+#include <assert.h>
+using namespace std;
+
+void testValue(bool booleano)
+{
+  assert(booleano);
+}
+void testValue(string mail)
+{
+  assert(mail == "pruebaqwerty@gmail.com");
+}
+void testValue(unsigned edad)
+{
+  assert(edad == 21);
+}
+void testValue(int calificacion)
+{
+  assert(calificacion == 10);
+}
+void testValue(double promedio)
+{
+  assert(promedio == 10.00);
+}
+
+int main() {
+    bool aprobado = true;
+    string mail = "pruebaqwertygmail.com";
+    mail.insert(11, 1, arroba);
+    testValue(mail);
+    unsigned edad = 21;
+    testValue(edad);
+    int calificacion = 10;
+    testValue(calificacion);
+    double promedio = 10.00;
+    testValue(promedio);
+    testValue(aprobado);
+}
