aa64cde99e66c9eed0f6e71f92db175f6d5f1bdc
Santiago1732 <49510623+Santiago1732@users.noreply.github.com>
2019-05-28 00:48:49 -0300

    Create adicion.cpp

---
+++ DD-Adición/adicion.cpp
@@ -0,0 +1,24 @@
+/*
+Trabajo Práctico 01: Adición
+Santiago Oliveira 16 8606 9
+28/05/2019
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+    int num2;
+
+    cout << "Ingrese un numero" << endl;
+    cin >> num1;
+    cout << "Ingrese otro numero" << endl;
+    cin >> num2;
+
+    cout << "La suma es: " <<num1+num2 << endl;
+
+    return 0;
+
+}
